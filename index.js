fe9faeff916845191920d9730ea28e11ca748374
Madison Evans <m.corbinevans@gmail.com>
2023-01-10 07:55:05 -0700

    completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const findMatching = (nameArray, input) => {
+  return nameArray.filter((name) => name.toUpperCase() === input.toUpperCase());
+};
+
+const fuzzyMatch = (nameArray, input) => {
+  if (nameArray.filter((name) => name.startsWith(input))) {
+    return nameArray.filter((name) => name.startsWith(input));
+  } else {
+    return [];
+  }
+};
+
+const matchName = (objArray, input) => {
+  return objArray.filter(
+    (obj) => obj.name.toUpperCase() === input.toUpperCase()
+  );
+};
